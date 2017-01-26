f3ddda575d14fb780a0b54c2721cc678281660b8
didinyah <dylanheitz@hotmail.fr>
2017-01-26 17:08:36 +0100

    Création de la classe GestionExcel et ajout de libraries

---
+++ src/mainpackage/GestionExcel.java
@@ -0,0 +1,31 @@
+package mainpackage;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+public class GestionExcel {
+public static void main(
+String[] args) {
+HSSFWorkbook wb = new HSSFWorkbook();
+HSSFSheet sheet = wb.createSheet("ma feuille");
+HSSFRow row = sheet.createRow(0);
+HSSFCell cell = row.createCell((short)0);
+cell.setCellValue(10);
+row.createCell((short)1).setCellValue(20);
+FileOutputStream fileOut;
+try {
+fileOut = new FileOutputStream("monfichier.xls");
+wb.write(fileOut);
+fileOut.close(); 
+} catch (FileNotFoundException e) {
+e.printStackTrace();
+} catch (IOException e) {
+e.printStackTrace();
+}
+}
+}
