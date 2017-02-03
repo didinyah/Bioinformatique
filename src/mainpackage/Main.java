9ec0ffd9c279b0e0084b3c76644709820bbe509b
Grohmann Nicolas <ni.grohmann@gmail.com>
2017-02-03 14:58:36 +0100

    Création de la classe Main qui contient le main
    Demandez moi si je dois ajouter des lignes(tests notamment)

---
+++ src/mainpackage/Main.java
@@ -0,0 +1,20 @@
+package mainpackage;
+
+/*
+ * Main
+ * Classe principale qui charge l'interface et lance les tests
+ */
+public class Main {
+	/*
+	 * Main
+	 * String[] args : arguments du programme (non utilisé)
+	 * Description : fonction principale du programme qui appelle l'interface 
+	 * et lance les tests
+	 * Return : void
+	 */
+	public static void main(String[] args) {
+		//loadInterface();
+		//testNucleotide();
+	}
+
+}
