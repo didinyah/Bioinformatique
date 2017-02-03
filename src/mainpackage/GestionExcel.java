cca6b21d41a543bf9a8466a5d8c68f0304b639dc
Bier Kevin <kevin.bier@orange.fr>
2017-02-03 09:36:05 +0100

    modification du gitignore parce que le classpath crée des conflits.

--- src/mainpackage/GestionExcel.java
+++ src/mainpackage/GestionExcel.java
@@ -9,23 +9,29 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 public class GestionExcel {
-public static void main(
-String[] args) {
-HSSFWorkbook wb = new HSSFWorkbook();
-HSSFSheet sheet = wb.createSheet("ma feuille");
-HSSFRow row = sheet.createRow(0);
-HSSFCell cell = row.createCell((short)0);
-cell.setCellValue(10);
-row.createCell((short)1).setCellValue(20);
-FileOutputStream fileOut;
-try {
-fileOut = new FileOutputStream("monfichier.xls");
-wb.write(fileOut);
-fileOut.close(); 
-} catch (FileNotFoundException e) {
-e.printStackTrace();
-} catch (IOException e) {
-e.printStackTrace();
-}
-}
+	public static void main(String[] args) 
+	{
+		HSSFWorkbook wb = new HSSFWorkbook();
+		HSSFSheet sheet = wb.createSheet("ma feuille");
+		HSSFRow row = sheet.createRow(0);
+		HSSFCell cell = row.createCell((short)0);
+		cell.setCellValue(10);
+		row.createCell((short)1).setCellValue(20);
+		FileOutputStream fileOut;
+		try 
+		{
+			fileOut = new FileOutputStream("monfichier.xls");
+			wb.write(fileOut);
+			fileOut.close(); 
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+		
+			e.printStackTrace();
+		}
+	}
 }
