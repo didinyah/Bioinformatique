cb08172860cfc77ad9c021fac9823df771c10fbb
Grohmann Nicolas <ni.grohmann@gmail.com>
2017-02-03 14:51:24 +0100

    Création de la classe de convention de nommage
    Référez-vous y pour que tout le monde nomme et commente de la même
    manière.

---
+++ src/mainpackage/Convention.java
@@ -0,0 +1,39 @@
+package mainpackage;
+
+/* Classe Convention
+ * Contient la nomenclature du projet
+ * 
+ * C'est à dire : comment nommer les classes, les fonctions et les variables
+ * ainsi que comment commenter.
+ */
+public class Convention {
+	//Variables
+	//Constante de test
+	private static int CONST = 0;
+	//Variable de test
+	private String nucleotide = "AAA";
+	
+	//Accesseurs
+	static int GetCONST(){
+		return CONST;
+	}
+	
+	String GetNucleotide(){
+		return this.nucleotide;
+	}
+	
+	void SetNucleotide(String n){
+		this.nucleotide = n;
+	}
+	
+	//Méthodes
+	/* 
+	 * checkNucleotide
+	 * String n : le nucléotide
+	 * Description : teste si un nucléotide est valide
+	 * Return : true si le nucléotide est valide, false sinon
+	 */
+	boolean checkNucleotide(String n){
+		return true;
+	}
+}
