66bc0ea78e96d3d876d6e40c38b718277a3454fe
didinyah <dylanheitz@hotmail.fr>
2017-02-17 05:01:31 +0100

    Ajustement des fonctions pour générer un excel et des formules correctes

--- src/mainpackage/GestionExcel.java
+++ src/mainpackage/GestionExcel.java
@@ -16,9 +16,11 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.formula.eval.NotImplementedException;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
 
 public class GestionExcel 
 {
+	// TYPES DE CELL :HSSFCell.[CELL_TYPE_FORMULA, CELL_TYPE_NUMERIC, ou CELL_TYPE_STRING]
 	
 	//Cr√©e un fichier excel vide
 	public static void CreationFichierVide(String file)
@@ -83,17 +85,45 @@ public class GestionExcel
 		}
 	}
 	
-	public static void CreationCellule(HSSFSheet sheet, String contenu, int ligne, int colonne)
+	public static void CreationCelluleText(HSSFSheet sheet, String contenu, int ligne, int colonne)
 	{
-		HSSFRow row = sheet.createRow(ligne);
+		HSSFRow row = sheet.getRow(ligne);
+		if (row == null) {
+			row = sheet.createRow(ligne);
+		}
 		HSSFCell cell = null;
 		cell = row.createCell((short) colonne);
 		cell.setCellValue(contenu);
 	}
 	
+	public static void CreationCelluleValeur(HSSFSheet sheet, int contenu, int ligne, int colonne)
+	{
+		HSSFRow row = sheet.getRow(ligne);
+		if (row == null) {
+			row = sheet.createRow(ligne);
+		}
+		HSSFCell cell = null;
+		cell = row.createCell((short) colonne);
+		cell.setCellValue(contenu);
+	}
+	
+	public static void CreationCelluleFormule(HSSFSheet sheet, String contenu, int ligne, int colonne)
+	{
+		HSSFRow row = sheet.getRow(ligne);
+		if (row == null) {
+			row = sheet.createRow(ligne);
+		}
+		HSSFCell cell = null;
+		cell = row.createCell((short) colonne);
+		cell.setCellFormula(contenu);
+	}
+	
 	public static void ViderCellule(HSSFSheet sheet, int ligne, int colonne)
 	{
-		HSSFRow row = sheet.createRow(ligne);
+		HSSFRow row = sheet.getRow(ligne);
+		if (row == null) {
+			row = sheet.createRow(ligne);
+		}
 		HSSFCell cell = null;
 		cell = row.createCell((short) colonne);
 		cell.setCellValue("");
@@ -101,7 +131,10 @@ public class GestionExcel
 	
 	public static void ColorierLigne(HSSFSheet sheet, int ligne, HSSFColor couleur, HSSFWorkbook wb)
 	{
-		HSSFRow row = sheet.createRow(ligne);
+		HSSFRow row = sheet.getRow(ligne);
+		if (row == null) {
+			row = sheet.createRow(ligne);
+		}
 		HSSFCellStyle cellStyle = wb.createCellStyle();
 		cellStyle.setFillBackgroundColor(couleur.getIndex());
 		row.setRowStyle(cellStyle);
@@ -125,18 +158,32 @@ public class GestionExcel
 		fonte.setFontName(police);
 		fonte.setColor(couleur.getIndex());
 	}
-	public static void RemplirLigne(HSSFSheet sheet, int ligne, int debutColonne, ArrayList<String> data)
+	public static void RemplirLigneText(HSSFSheet sheet, int ligne, int debutColonne, ArrayList<String> data)
 	{
-		for(int i =debutColonne; i < data.size(); i++)
+		for(int i =0; i < data.size(); i++)
 		{
-			CreationCellule(sheet, data.get(i), ligne, i);
+			CreationCelluleText(sheet, data.get(i), ligne, i+debutColonne);
 		}
 	}
-	public static void RemplirColonne(HSSFSheet sheet, int colonne, int debutLigne, ArrayList<String>data)
+	public static void RemplirColonneText(HSSFSheet sheet, int colonne, int debutLigne, ArrayList<String>data)
 	{
-		for(int i =debutLigne; i < data.size(); i++)
+		for(int i =0; i < data.size(); i++)
 		{
-			CreationCellule(sheet, data.get(i), i, colonne);
+			CreationCelluleText(sheet, data.get(i), i+debutLigne, colonne);
+		}
+	}
+	public static void RemplirLigneValeurs(HSSFSheet sheet, int ligne, int debutColonne, ArrayList<Integer> data)
+	{
+		for(int i =0; i < data.size(); i++)
+		{
+			CreationCelluleValeur(sheet, data.get(i), ligne, i+debutColonne);
+		}
+	}
+	public static void RemplirColonneValeurs(HSSFSheet sheet, int colonne, int debutLigne, ArrayList<Integer>data)
+	{
+		for(int i =0; i < data.size(); i++)
+		{
+			CreationCelluleValeur(sheet, data.get(i), i+debutLigne, colonne);
 		}
 	}
 	public static void SupprimerLigne(HSSFSheet sheet, int ligne)
@@ -161,10 +208,21 @@ public class GestionExcel
 	public static void main(String[] args) 
 	{
 		HSSFWorkbook wb = new HSSFWorkbook();
-		HSSFSheet sheet = wb.createSheet("ma feuille");
+		HSSFSheet sheet = wb.createSheet("mafeuille");
 		ArrayList<String> trinucleotides = Utils.getListOfTriNucleotideCAPSLOCK();
-		RemplirColonne(sheet, 0, 1, trinucleotides);
-
-		CreationFichier("bwaaa.xlsx", wb);
+		RemplirColonneText(sheet, 0, 1, trinucleotides);
+		ArrayList<Integer> testNombres = new ArrayList<Integer>();
+		for(int i=0; i<64; i++) {
+			int temp = i*i;
+			testNombres.add(temp);
+		}
+		RemplirColonneValeurs(sheet, 1, 1, testNombres);
+		CreationCelluleText(sheet, "Total", 65, 0);
+		CreationCelluleFormule(sheet, "SUM(B2:B65)", 65, 1);
+		CreationFichier("bwaaa.xls", wb);
+		/* pour Ítre s˚r que les rÈsultats des formules soient update en temps rÈel
+		 * 
+		 * FormulaEvaluator evaluator = wb.getCreationHelper().createFormulaEvaluator();
+		evaluator.evaluateAll();*/
 	}
 }
