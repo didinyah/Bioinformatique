cf543ba227b354124fc584002176c14a14a625af
Morineau Charlelie <charlelie.morineau@hotmail.fr>
2017-02-17 14:16:50 +0100

    Fonctions moi

---
+++ src/mainpackage/Charlelie.java
@@ -0,0 +1,74 @@
+package mainpackage;
+
+import java.util.ArrayList;
+
+public class Charlelie {
+
+	public static boolean checkString (String string1, String string2){
+		if (string1.length()!=string2.length()){
+			return false;
+		}
+		else {
+			for (int i=0; i<string1.length();i++){
+				if (string1.charAt(i)!=string2.charAt(i)){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public static boolean checkIsCDS (String line){
+		
+		
+		
+		return true;	
+	}
+	
+	public static boolean checkCodonInit (String line){
+		ArrayList<String> list = new ArrayList<String>();
+		boolean b = true;
+		for (int i=0; i<list.size();i++){
+			for (int j=0; j<list.get(i).length();j++){
+				if (line.charAt(j)!= list.get(i).charAt(j)){
+					b = false;
+				}
+				if (b==true){
+					return true;
+				}
+				else{
+					b = true;
+				}
+			}
+		}
+		return false; 
+	}
+	
+	public static boolean checkCodonStop (String line){
+		ArrayList<String> list = new ArrayList<String>();
+		boolean b = true;
+		for (int i=0; i<list.size();i++){
+			for (int j=0; j<list.get(i).length();j++){
+				if (line.charAt(j)!= list.get(i).charAt(j)){
+					b = false;
+				}
+				if (b==true){
+					return true;
+				}
+				else{
+					b = true;
+				}
+			}
+		}
+		return false; 
+	}
+	
+	public static boolean checkCorrectChar (char c){
+		if (c=='a' || c=='c' || c=='t' || c=='g'){
+			return true;
+		}
+		return false;
+	}
+	
+	
+}
