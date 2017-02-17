e0f9e9d4825f04c57f02cb00fd036ae91cccbba6
Bier Kevin <kevin.bier@orange.fr>
2017-02-17 14:37:17 +0100

    Passage de HSSF à XSSF pour pouvoir créer des xlsx.

--- src/mainpackage/GestionExcel.java
+++ src/mainpackage/GestionExcel.java
@@ -8,15 +8,23 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
+/*import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.hssf.util.HSSFColor;*/
+import org.apache.poi.xssf.*;
 import org.apache.poi.ss.formula.eval.NotImplementedException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class GestionExcel 
 {
@@ -25,7 +33,7 @@ public class GestionExcel
 	//CrÃ©e un fichier excel vide
 	public static void CreationFichierVide(String file)
 	{
-		HSSFWorkbook wb = new HSSFWorkbook();
+		XSSFWorkbook wb = new XSSFWorkbook();
 		FileOutputStream fileOut;
 		try 
 		{
@@ -44,7 +52,7 @@ public class GestionExcel
 		}
 	}
 	
-	public static void CreationFichier(String file, HSSFWorkbook wb)
+	public static void CreationFichier(String file, XSSFWorkbook wb)
 	{
 		FileOutputStream fileOut;
 		try 
@@ -85,130 +93,130 @@ public class GestionExcel
 		}
 	}
 	
-	public static void CreationCelluleText(HSSFSheet sheet, String contenu, int ligne, int colonne)
+	public static void CreationCelluleText(XSSFSheet sheet, String contenu, int ligne, int colonne)
 	{
-		HSSFRow row = sheet.getRow(ligne);
+		Row row = sheet.getRow(ligne);
 		if (row == null) {
 			row = sheet.createRow(ligne);
 		}
-		HSSFCell cell = null;
+		Cell cell = null;
 		cell = row.createCell((short) colonne);
 		cell.setCellValue(contenu);
 	}
 	
-	public static void CreationCelluleValeur(HSSFSheet sheet, int contenu, int ligne, int colonne)
+	public static void CreationCelluleValeur(XSSFSheet sheet, int contenu, int ligne, int colonne)
 	{
-		HSSFRow row = sheet.getRow(ligne);
+		Row row = sheet.getRow(ligne);
 		if (row == null) {
 			row = sheet.createRow(ligne);
 		}
-		HSSFCell cell = null;
+		Cell cell = null;
 		cell = row.createCell((short) colonne);
 		cell.setCellValue(contenu);
 	}
 	
-	public static void CreationCelluleFormule(HSSFSheet sheet, String contenu, int ligne, int colonne)
+	public static void CreationCelluleFormule(XSSFSheet sheet, String contenu, int ligne, int colonne)
 	{
-		HSSFRow row = sheet.getRow(ligne);
+		Row row = sheet.getRow(ligne);
 		if (row == null) {
 			row = sheet.createRow(ligne);
 		}
-		HSSFCell cell = null;
+		Cell cell = null;
 		cell = row.createCell((short) colonne);
 		cell.setCellFormula(contenu);
 	}
 	
-	public static void ViderCellule(HSSFSheet sheet, int ligne, int colonne)
+	public static void ViderCellule(XSSFSheet sheet, int ligne, int colonne)
 	{
-		HSSFRow row = sheet.getRow(ligne);
+		Row row = sheet.getRow(ligne);
 		if (row == null) {
 			row = sheet.createRow(ligne);
 		}
-		HSSFCell cell = null;
+		Cell cell = null;
 		cell = row.createCell((short) colonne);
 		cell.setCellValue("");
 	}
 	
-	public static void ColorierLigne(HSSFSheet sheet, int ligne, HSSFColor couleur, HSSFWorkbook wb)
+	public static void ColorierLigne(XSSFSheet sheet, int ligne, IndexedColors couleur, XSSFWorkbook wb)
 	{
-		HSSFRow row = sheet.getRow(ligne);
+		Row row = sheet.getRow(ligne);
 		if (row == null) {
 			row = sheet.createRow(ligne);
 		}
-		HSSFCellStyle cellStyle = wb.createCellStyle();
+		CellStyle cellStyle = wb.createCellStyle();
 		cellStyle.setFillBackgroundColor(couleur.getIndex());
 		row.setRowStyle(cellStyle);
 	}
 	
-	public static void ColorierColonne(HSSFSheet sheet, int colonne, HSSFColor couleur, HSSFWorkbook wb)
+	public static void ColorierColonne(XSSFSheet sheet, int colonne, IndexedColors couleur, XSSFWorkbook wb)
 	{
-		HSSFRow row = null;
-		HSSFCellStyle cellStyle = wb.createCellStyle();
+		Row row = null;
+		CellStyle cellStyle = wb.createCellStyle();
 		cellStyle.setFillBackgroundColor(couleur.getIndex());
 		for (Iterator rowIt = sheet.rowIterator(); rowIt.hasNext();) 
 		{
-			row = (HSSFRow) rowIt.next();
+			row = (Row) rowIt.next();
 			row.getCell(0).setCellStyle(cellStyle);
 		}
 	}
-	public static void ModifierPolice(HSSFSheet sheet, int ligne, int colonne, String police, int taille, HSSFColor couleur, HSSFWorkbook wb)
+	public static void ModifierPolice(XSSFSheet sheet, int ligne, int colonne, String police, int taille, IndexedColors couleur, XSSFWorkbook wb)
 	{
-		HSSFFont fonte = wb.createFont();
+		XSSFFont fonte = wb.createFont();
 		fonte.setFontHeightInPoints((short) taille);
 		fonte.setFontName(police);
 		fonte.setColor(couleur.getIndex());
 	}
-	public static void RemplirLigneText(HSSFSheet sheet, int ligne, int debutColonne, ArrayList<String> data)
+	public static void RemplirLigneText(XSSFSheet sheet, int ligne, int debutColonne, ArrayList<String> data)
 	{
 		for(int i =0; i < data.size(); i++)
 		{
 			CreationCelluleText(sheet, data.get(i), ligne, i+debutColonne);
 		}
 	}
-	public static void RemplirColonneText(HSSFSheet sheet, int colonne, int debutLigne, ArrayList<String>data)
+	public static void RemplirColonneText(XSSFSheet sheet, int colonne, int debutLigne, ArrayList<String>data)
 	{
 		for(int i =0; i < data.size(); i++)
 		{
 			CreationCelluleText(sheet, data.get(i), i+debutLigne, colonne);
 		}
 	}
-	public static void RemplirLigneValeurs(HSSFSheet sheet, int ligne, int debutColonne, ArrayList<Integer> data)
+	public static void RemplirLigneValeurs(XSSFSheet sheet, int ligne, int debutColonne, ArrayList<Integer> data)
 	{
 		for(int i =0; i < data.size(); i++)
 		{
 			CreationCelluleValeur(sheet, data.get(i), ligne, i+debutColonne);
 		}
 	}
-	public static void RemplirColonneValeurs(HSSFSheet sheet, int colonne, int debutLigne, ArrayList<Integer>data)
+	public static void RemplirColonneValeurs(XSSFSheet sheet, int colonne, int debutLigne, ArrayList<Integer>data)
 	{
 		for(int i =0; i < data.size(); i++)
 		{
 			CreationCelluleValeur(sheet, data.get(i), i+debutLigne, colonne);
 		}
 	}
-	public static void SupprimerLigne(HSSFSheet sheet, int ligne)
+	public static void SupprimerLigne(XSSFSheet sheet, int ligne)
 	{
-		HSSFRow row = sheet.getRow(ligne);
-		HSSFCell cell = null;
+		Row row = sheet.getRow(ligne);
+		Cell cell = null;
 		for (Iterator cellIt = row.cellIterator(); cellIt.hasNext();) 
 		{
-			cell = (HSSFCell) cellIt.next();
+			cell = (Cell) cellIt.next();
 			cell.setCellValue("");
 		}
 	}
-	public static void SupprimerColonne(HSSFSheet sheet, int colonne)
+	public static void SupprimerColonne(XSSFSheet sheet, int colonne)
 	{
-		HSSFRow row = null;
+		Row row = null;
 		for (Iterator rowIt = sheet.rowIterator(); rowIt.hasNext();) 
 		{
-			row = (HSSFRow) rowIt.next();
+			row = (Row) rowIt.next();
 			row.getCell(0).setCellValue("");
 		}
 	}
 	public static void main(String[] args) 
 	{
-		HSSFWorkbook wb = new HSSFWorkbook();
-		HSSFSheet sheet = wb.createSheet("mafeuille");
+		XSSFWorkbook wb = new XSSFWorkbook();
+		XSSFSheet sheet = wb.createSheet("mafeuille");
 		ArrayList<String> trinucleotides = Utils.getListOfTriNucleotideCAPSLOCK();
 		RemplirColonneText(sheet, 0, 1, trinucleotides);
 		ArrayList<Integer> testNombres = new ArrayList<Integer>();
@@ -219,8 +227,8 @@ public class GestionExcel
 		RemplirColonneValeurs(sheet, 1, 1, testNombres);
 		CreationCelluleText(sheet, "Total", 65, 0);
 		CreationCelluleFormule(sheet, "SUM(B2:B65)", 65, 1);
-		CreationFichier("bwaaa.xls", wb);
-		/* pour être sûr que les résultats des formules soient update en temps réel
+		CreationFichier("bwaaa.xlsx", wb);
+		/* pour ï¿½tre sï¿½r que les rï¿½sultats des formules soient update en temps rï¿½el
 		 * 
 		 * FormulaEvaluator evaluator = wb.getCreationHelper().createFormulaEvaluator();
 		evaluator.evaluateAll();*/
