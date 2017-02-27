be2c899cb8ffa98e1ba11bd86d00e17ccb227b00
Lebrun Magdeleine <magdeleine.lebrun@gmail.com>
2017-02-27 18:35:05 +0100

    Fonction checkIsCds

--- src/mainpackage/TestMag.java
+++ src/mainpackage/TestMag.java
@@ -12,5 +12,15 @@ public class TestMag extends Mag {
 		assertEquals("Failure : Complement", "a", Mag.Complement("t"));
 		assertEquals("Failure : Complement", "ttcgctaaac", Mag.Complement("gtttagcgaa"));
 	}
+	
+	@Test
+	public void testCheckIsCds() throws Exception {
+		assertTrue("Failure : check is cds", Mag.checkIsCDS("     CDS             complement(388..813)"));
+		assertTrue("Failure : check is cds", Mag.checkIsCDS("     CDS             complement(1206..2420)"));
+		assertFalse("Failure : check is cds", Mag.checkIsCDS("     CDS             complement(2731..2420)"));
+		assertFalse("Failure : check is cds", Mag.checkIsCDS("     CDS             <143859..144731"));
+		assertFalse("Failure : check is cds", Mag.checkIsCDS("     CDS             4064..5311"));
+		assertFalse("Failure : check is cds", Mag.checkIsCDS("     CDS             LPAGVESVRVVSRSSRPC"));
+	}
 
 }
