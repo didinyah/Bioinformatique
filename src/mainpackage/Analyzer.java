db94eadf86b3187aaf54fc569ff2165b89487cd6
Poussin <alexandre.sandros@gmail.com>
2017-03-01 15:12:53 +0100

    Ajout de la fonction cdsToBorne qui prend un cds (normal ou multiline) et envoie une liste de borne (soit 1 élément soit n fois multiline élément) Ajout de nombreux test dans TestScripts sur des exemples réel et sur des exemples théorique pour l'extraction de cds voir testCdsExtractor() (il y a 2 cas qui sont encore en todo)

--- src/mainpackage/Analyzer.java
+++ src/mainpackage/Analyzer.java
@@ -4,6 +4,7 @@ import jdk.nashorn.internal.runtime.ECMAException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -45,24 +46,6 @@ public class Analyzer {
                 }
             }
         }
-        return true;
-    }
-
-    /*
-    * Les bornes inf et sup existent dans la séquence
-    * Les bornes inf et sup sont des nombres
-    * La borne inf est inf à la borne sup
-    * les bornes inf et sup sont séparées par ..
-    */
-    public static boolean checkIsCDS (String line){
-
-
-        // TODO revient à lire listBorne est vérifier si on est compris dans une borne
-        // deux problèmatique soit on considère que listBorn est trié en fonction de la premier borne par construction
-        // soit pas (temps de calcul très long)
-
-        //On ne doit pas vérifier ici si les bornes sont conformes par construction c'est bon
-
         return true;
     }
 
@@ -170,13 +153,11 @@ public class Analyzer {
                 String[] items = p2.split(tmp);
                 for ( String it : items){
                     Borne b = stringToBorne(it);
-                    System.out.println(b);
                     listTmp.add(b);
                 }
             }else{
-                // cas ou il n'y en pas
+                // cas ou il n'y en pas (todo cas inutile ???)
                 Borne b = stringToBorne(tmp);
-                System.out.println(b);
                 listTmp.add(b);
             }
 
@@ -186,6 +167,10 @@ public class Analyzer {
         return listTmp;
     }
 
+    public static boolean isJoin(String line){
+        Pattern p = Pattern.compile("join\\(.*\\)");
+        return p.matcher(line).find();
+    }
     /*
     * Fonction qui doit tester et extraire si une chaine est un intervalle de la forme : a..b
     *    - a<b
@@ -193,11 +178,11 @@ public class Analyzer {
     *    - b est un int
     *    renvoi Borne a b
     *    Rien d'autres
-    *    TODO vérifier les expections génré sont-elles les bonnes ?
+    *    TODO vérifier les expections génèré sont-elles les bonnes ?
     */
     public static Borne stringToBorne(String it) throws Exception {
     // Prend un string de type int..int et renvoie une Borne en vérifiant si tout est bon
-        Pattern p = Pattern.compile("([0-9]+)\\.\\.([0-9]+)");
+        Pattern p = Pattern.compile("^([0-9]+)\\.\\.([0-9]+)$");
         Matcher m = p.matcher(it);
         if(m.find()){
             if(m.groupCount() == 2){
@@ -222,23 +207,95 @@ public class Analyzer {
     }
 
     public static List<Borne> cdsToBornes(String cdsLine) throws Exception {
-        // TODO à finir stopé car multiligne cds detecté provoque bug et donc revnir à la source du pb avant !
         Pattern p = Pattern.compile(" *CDS +(.*)");
         Matcher m = p.matcher(cdsLine);
         if(m.find()){
             String strToExtract = m.group(1);
             if(strToExtract.length() > 2){// car il y a au minimun ..
-                // le cas ou y a complement (join -> appeler join
-                // le cas join -> join
-                // le cas ou y a juste complement ( appeler stringtoborne
-                // le cas simple
+                Pattern pcomplement = Pattern.compile("complement\\((.*)\\)");
+                Matcher mcomplement = pcomplement.matcher(strToExtract);
+                if(mcomplement.find()){
+                    strToExtract = mcomplement.group(1); // on enlève le texte complement( )
+                    if(isJoin(strToExtract)){
+                        // on crée l'unique id identifiant la même jointure
+                        String uniqueID = UUID.randomUUID().toString();
+                        List<Borne> joinList =  join(strToExtract);
 
+                        if(isCorrectListBorne(joinList)){
+                            // On ajoute des informations sur les bornes
+                            for(Borne b :joinList)
+                            {
+                                b.setComplement(true);
+                                b.setMultipleBorne(true);
+                                b.setLinkId(uniqueID);
+                            }
+                            return joinList;
+                        }else{
+                            throw new Exception();
+                        }
+                    }else{
+                        Borne b = stringToBorne(strToExtract);
+                        b.setComplement(true);
+                        List<Borne> singleElement = new ArrayList<Borne>();
+                        singleElement.add(b);
+                        return singleElement;
+                    }
+                }else{
+                    if(isJoin(strToExtract)){
+                        // on crée l'unique id identifiant la même jointure
+                        String uniqueID = UUID.randomUUID().toString();
+                        List<Borne> joinList =  join(strToExtract);
+
+                        if(isCorrectListBorne(joinList)){
+                        // On ajoute des informations sur les bornes
+                            for(Borne b :joinList)
+                            {
+                                b.setMultipleBorne(true);
+                                b.setLinkId(uniqueID);
+                            }
+                            return joinList;
+                        }else{
+                            throw new Exception();
+                        }
+                    }else{
+                        Borne b = stringToBorne(strToExtract);
+                        List<Borne> singleElement = new ArrayList<Borne>();
+                        singleElement.add(b);
+                        return singleElement;
+                    }
+                }
+            }else{
+                throw new Exception();
             }
         }else{
             throw new Exception();
         }
-        return null;
     }
+
+    private static boolean isCorrectListBorne(List<Borne> joinList) {
+        if(!joinList.isEmpty()){
+            Borne lastBorne = null;
+            for(Borne b: joinList){
+                if(!b.isCorrectBorne()){
+                    return false;
+                }
+                if(lastBorne == null){
+                    lastBorne =b;
+                }else{
+                    if(!b.isGreaterThan(lastBorne)){
+                        return false;
+                    }
+                    lastBorne = b;
+                }
+
+            }
+        }else{
+            return false;
+        }
+
+        return  true;
+    }
+
 
     public static boolean isCdsMultiLine(String line1) {
         // Il faut qu'il y a CDS dans la ligne et que la dernier caractère soit une virgule ,
@@ -272,6 +329,24 @@ public class Analyzer {
     }
 
     private static class Borne {
+
+        private Integer borninf;
+        private Integer bornsup;
+        private boolean complement; // savoir si la borne est complement
+        private boolean multipleBorne; // savoir si c'est une borne d'une jointure
+        private String  linkId; // l'id de lien avec les autres borne de la même jointure (todo créer un id plus complexe uniqid)
+
+        public Borne(){
+            // Variable par défaut
+            complement = false;
+            multipleBorne = false;
+            linkId = "error"; // -1 signifie qu'il n'y a pas de lien
+        }
+        public boolean isInBorne(int a){
+            return borninf <= a && a <= bornsup;
+        }
+
+
         public void setBorninf(Integer borninf) {
             this.borninf = borninf;
         }
@@ -280,18 +355,59 @@ public class Analyzer {
             this.bornsup = bornsup;
         }
 
-        private Integer borninf;
-        private Integer bornsup;
 
-        public boolean isInBorne(int a){
-            return borninf <= a && a <= bornsup;
+        @Override
+        // i = inf s =sup C = complement M = multiline
+        public String toString() {
+            String complementLetter = complement ? "C": "";
+            String multiLine = multipleBorne ? "M": "";
+            String addInfo = complement || multipleBorne ? " (I): " + complementLetter +" "+ multiLine : "";
+            return borninf+ " i .. s "+bornsup + addInfo ;
+        }
+
+        public void setComplement(boolean complement) {
+            this.complement = complement;
         }
 
 
+        public void setMultipleBorne(boolean multipleBorne) {
+            this.multipleBorne = multipleBorne;
+        }
 
-        @Override
-        public String toString() {
-            return borninf+ " i .. s "+bornsup;
+        public boolean isComplement() {
+            return complement;
+        }
+
+        public boolean isMultipleBorne() {
+            return multipleBorne;
+        }
+
+        public String getLinkId() {
+            return linkId;
+        }
+
+        public void setLinkId(String linkId) {
+            this.linkId = linkId;
+        }
+
+        public boolean isCorrectBorne(){
+            if(borninf < bornsup){
+                // test sur complement ?
+                if(multipleBorne){
+                    // on test que l'unique ID a bien été défini
+                    return !linkId.equals("error");
+                }else{
+                    return true;
+                }
+
+            }else{
+                return false;
+            }
+        }
+
+        // b1.isGreaterThan(b2) => b1.borninf >= b2.bornsup (todo vérifier >= ou > stric ? autre à verifier ?)
+        public boolean isGreaterThan(Borne b){
+            return borninf >= b.bornsup;
         }
     }
 
--- src/mainpackage/TestScript.java
+++ src/mainpackage/TestScript.java
@@ -65,44 +65,82 @@ public class TestScript {
 
     public static void testCdsExtractor(){
         // Extrait d'un fichier d'exemple
-        // les 5 premières lignes sont apriori bonne
-        // la 6 eme est un test de fail car le symbole < est présent (PS si vous trouvez des CDS particuliers vous pouvez les insérer ici pour voir si sa work)
-        // le 7eme est pour tester un bug possible car dans la séquence suivante il y a le mot CDS
-        // le 8eme est un fail car join plus petit que l'autre
+        // success test
         List<String> listLineCds = new ArrayList<String>();
         listLineCds.add("     CDS             complement(388..813)");
         listLineCds.add("     CDS             complement(1206..2420)");
         listLineCds.add("     CDS             complement(2681..3160)");
         listLineCds.add("     CDS             3400..3927");
         listLineCds.add("     CDS             4064..5311");
-        listLineCds.add("     CDS             <143859..144731");
-        listLineCds.add("                     KLITNSGATIYPFKKSENIYSFMLPAGVESVRVVSRSSRPCDSIGPFVDDRRQMGVAV");
+        // cas 2
+        listLineCds.add("     CDS             join(1950..2004,2076..2861,2933..3211,3281..3291," +
+                "3343..3597,3670..4362)");// fuuu
+        //cas 3
+        listLineCds.add("     CDS             join(104713..104780,104838..105015,105068..105285," +
+                "105347..105480,105539..105774,105825..106013," +
+                "106073..106180,106317..106559,106628..106828)");
+        //cas 4 (conclusion à généraliser)
+        listLineCds.add("     CDS             join(172641..172693,172748..172841,172899..173051," +
+                "173101..173382,173442..173548,173598..173839," +
+                "173895..174048,174102..174369,174425..174639," +
+                "174697..174820,174906..175134,175196..175275)");
+        // mix complement join 4 line l'enfer
+        listLineCds.add("     CDS             complement(join(20208..20521,20614..20708,20772..20895," +
+                "20954..21169,21231..21240,21292..21353,21408..21492," +
+                "21544..21910,21956..21982,22036..22214,22265..22447," +
+                "22497..22551,22601..23079,23131..23183,23237..23912))");
+
+
+        // crash test :
+        listLineCds.add("     CDS             <143859..144731"); // line
+        listLineCds.add("                     KLITNSGATIYPFKKSENIYSFMLPAGVESVRVVSRSSRPCDSIGPFVDDRRQMGVAV"); //line 8
         listLineCds.add("     CDS             complement(join(182789..182940,1..571))");
         listLineCds.add("     CDS             join(54151..54545,54597..54999,55107..>55883)");
-        listLineCds.add("     CDS             complement(join(<9538..9972,10012..10266,10843..11391))\n");
-        // TODO comment interpreter ces cas là sur plusieurs lignes? pendant le reader line car sa saute actuellement la deuxième partie
-        // cas 2
-        listLineCds.add("     CDS             join(1950..2004,2076..2861,2933..3211,3281..3291,\n" +
-                "                     3343..3597,3670..4362)");// fuuu
-        //cas 3
-        listLineCds.add("     CDS             join(104713..104780,104838..105015,105068..105285,\n" +
-                "                     105347..105480,105539..105774,105825..106013,\n" +
-                "                     106073..106180,106317..106559,106628..106828)");
-        //cas 4 (conclusion à généraliser)
-        listLineCds.add("     CDS             join(172641..172693,172748..172841,172899..173051,\n" +
-                "                     173101..173382,173442..173548,173598..173839,\n" +
-                "                     173895..174048,174102..174369,174425..174639,\n" +
-                "                     174697..174820,174906..175134,175196..175275)");
-        // mix complement join 4 line l'enfer
-        listLineCds.add("     CDS             complement(join(20208..20521,20614..20708,20772..20895,\n" +
-                "                     20954..21169,21231..21240,21292..21353,21408..21492,\n" +
-                "                     21544..21910,21956..21982,22036..22214,22265..22447,\n" +
-                "                     22497..22551,22601..23079,23131..23183,23237..23912))");
+        listLineCds.add("     CDS             complement(join(<9538..9972,10012..10266,10843..11391))");
+        listLineCds.add("     CDS             complement(join(20208..20521,20614..20708,20772..20895," +
+                "20954..21169,21231..21240,21292..21353,21408..21492," +
+                "21544..21910,21956..21982,22036..5,22265..22447," + // un piège ici ->5<-
+                "22497..22551,22601..23079,23131..23183,23237..23912))");
+        listLineCds.add("     CDS             join(1950..2004,2000..2861)"); // chevauchement
 
 
-        // TODO fonction check + extract info par ligne
-        // TODO créer une list de Borne de cds (Voir Analyzer Borne)
-        // TODO test pour savoir si CheckIsInCds fonctionne
+        // test pas sur du résultat TODO à confirmer
+        listLineCds.add("     CDS             join( 1950..2004,2000..2861)"); // crash car espace
+        listLineCds.add("     CDS             4064 .. 5311"); // crash car espace
+        listLineCds.add("     CDS             complement( 2681..3160 )");
+        listLineCds.add("     CDS             join(complement(2681..3160))"); // crash car  inverser join complement 'test théorique"
+        listLineCds.add("     CDS             join(2681..3160)"); // le dernier n'est pas considerer comme un bug pourtant un seul join sa parrait bizzare (TODO à verifier)
+        listLineCds.add("     CDS             complement(join(2681..3160))"); // de même pour lui bizarre (il marche)
+
+        int count_iscds = 0;
+        int count_isnotcds =0;
+        int crash_count = 0;
+        int succes_count = 0;
+        int i= 0;
+        for(String cdsLine : listLineCds){
+            i++;
+            if(Analyzer.checkCds(cdsLine)){
+                count_iscds++;
+            }else{
+                count_isnotcds++;
+            }
+            try {
+                Analyzer.cdsToBornes(cdsLine);
+                succes_count++;
+
+            } catch (Exception e) {
+                crash_count++;
+
+                System.out.println("line crash :"+i  + " \""+cdsLine+"\"");
+            }
+        }
+
+        System.out.println("********BILAN***********");
+        System.out.println("Count is cds : "+count_iscds);
+        System.out.println("Count is not cds : "+count_isnotcds);
+        System.out.println("Test is true cds : "+succes_count);
+        System.out.println("\t is fake cds : "+crash_count);
+
 
 
 
@@ -116,14 +154,7 @@ public class TestScript {
 
         System.out.println("***************************************");
 
-        testFile();
-
-
-        try {
-            Analyzer.join("join(50..60,80..90)");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        testCdsExtractor();
 
 
     }
